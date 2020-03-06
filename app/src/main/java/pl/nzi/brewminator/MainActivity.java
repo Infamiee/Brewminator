e245120914b4dbee3c99551d48454d59fb680b27
Jakub Kowalczyk <kowalczykj@protonmail.com>
2020-03-06 23:15:22 +0100

    BREW-37 Strona startowa

--- app/src/main/java/pl/nzi/brewminator/MainActivity.java
+++ app/src/main/java/pl/nzi/brewminator/MainActivity.java
@@ -2,12 +2,15 @@ package pl.nzi.brewminator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     AnimationDrawable animationDrawable;
+    private static int SPLASH_SCREEN_OUT=2750;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = (ImageView) findViewById(R.id.imageView4);
         animationDrawable = (AnimationDrawable) imageView.getDrawable();
         animationDrawable.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_OUT);
 
     }
 }
