98d8dedf6ad79fce5eb1efa577a9680fa03a74bb
Jakub Kowalczyk <kowalczykj@protonmail.com>
2020-03-06 22:38:16 +0100

    added loading logo and loading animation

---
+++ app/src/main/java/pl/nzi/brewminator/MainActivity.java
@@ -0,0 +1,20 @@
+package pl.nzi.brewminator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    AnimationDrawable animationDrawable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView4);
+        animationDrawable = (AnimationDrawable) imageView.getDrawable();
+        animationDrawable.start();
+
+    }
+}
