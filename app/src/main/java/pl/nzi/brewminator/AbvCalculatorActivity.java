e644937b1a833f43443315fff60060446a88cd55
Jakub Kowalczyk <kowalczykj@protonmail.com>
2020-03-19 23:24:08 +0100

    BREW-37 Strona startowa

---
+++ app/src/main/java/pl/nzi/brewminator/AbvCalculatorActivity.java
@@ -0,0 +1,47 @@
+package pl.nzi.brewminator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import pl.nzi.brewminator.calculator.AbvCalculator;
+import pl.nzi.brewminator.exception.WrongGravitiesException;
+
+public class AbvCalculatorActivity extends AppCompatActivity {
+    TextView og;
+    TextView fg;
+    Button calculate;
+    AbvCalculator abvCalculator = new AbvCalculator();
+    TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abv_calculator);
+        og = (TextView) findViewById(R.id.og);
+        fg = (TextView) findViewById(R.id.fg);
+        calculate = (Button) findViewById(R.id.calculate);
+        result = (TextView) findViewById(R.id.result);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v != null) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                try {
+                    double res = abvCalculator.calculate(Double.parseDouble(og.getText().toString()),Double.parseDouble(fg.getText().toString()));
+                    result.setText(String.format("%.2f",res)+" %");
+                }catch (WrongGravitiesException e){
+                    result.setText(e.getMessage());
+                }
+
+
+            }
+        });
+    }
+}
--- app/src/main/java/pl/nzi/brewminator/HomeActivity.java
+++ app/src/main/java/pl/nzi/brewminator/HomeActivity.java
@@ -2,13 +2,33 @@ package pl.nzi.brewminator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import pl.nzi.brewminator.calculator.AbvCalculator;
+
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        button = (Button) findViewById(R.id.butt1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, AbvCalculatorActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
 }
