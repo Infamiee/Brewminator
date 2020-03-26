e5f8ffd5b06c43dbad7a9e6941bcd997f22068c4
Jakub Kowalczyk <kowalczykj@protonmail.com>
2020-03-26 19:22:02 +0100

    BREW-29 Frontend Kalkulatora ABV

--- app/src/main/java/pl/nzi/brewminator/HomeActivity.java
+++ app/src/main/java/pl/nzi/brewminator/HomeActivity.java
@@ -1,6 +1,8 @@
 package pl.nzi.brewminator;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +22,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         button = (Button) findViewById(R.id.butt1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
