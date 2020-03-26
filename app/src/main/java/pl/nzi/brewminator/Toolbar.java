5effe767b46e29e7537eccb21c9d6dbb86ee4f0f
Jakub Kowalczyk <kowalczykj@protonmail.com>
2020-03-26 19:20:05 +0100

    Merge branch 'BREW-40' of C:\Users\Kuba\AndroidStudioProjects\Brewminator with conflicts.

---
+++ app/src/main/java/pl/nzi/brewminator/Toolbar.java
@@ -0,0 +1,8 @@
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Toolbar extends AppCompatActivity {
+    Button search = (Button) findViewById(R.id.)
+
+}
