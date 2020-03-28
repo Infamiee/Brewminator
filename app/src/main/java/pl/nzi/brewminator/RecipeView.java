90a1ac766b6fff2cd74d8cc84635285e5dc07983
Jakub Kowalczyk <kowalczykj@protonmail.com>
2020-03-28 23:33:30 +0100

    BREW-17 Połączenie Android studio z api

---
+++ app/src/main/java/pl/nzi/brewminator/RecipeView.java
@@ -0,0 +1,35 @@
+package pl.nzi.brewminator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class RecipeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view2);
+
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("id",0);
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.1.18:5000/recipe?id="+String.valueOf(id);
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    System.out.println(response);
+                }, Throwable::printStackTrace);
+
+        queue.add(stringRequest);
+    }
+}
