05460d44ff6f1f3b9ad8a68b8bc763676bc7659c
Jakub Kowalczyk <kowalczykj@protonmail.com>
2020-04-15 22:37:19 +0200

    BREW-36 Backend

--- app/src/main/java/pl/nzi/brewminator/service/RecipeSteps.java
+++ app/src/main/java/pl/nzi/brewminator/service/RecipeSteps.java
@@ -1,22 +1,72 @@
 package pl.nzi.brewminator.service;
 
-import com.digidemic.unitof.S;
+import android.os.AsyncTask;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import pl.nzi.brewminator.model.MASHSTEP;
+import pl.nzi.brewminator.model.MISC;
 import pl.nzi.brewminator.model.Recipe;
 
 public class RecipeSteps {
+
+
     enum STEPS{
         PREP,MASH,BOIL,FERMENT
     }
-    Recipe recipe;
-
+    private Recipe recipe;
+    private int boilTime;
+    private int mashTime;
+    private List<MISC> fermentMisc;
+    private List<MISC> boilMisc;
+    private List<MISC> mashMisc;
     public RecipeSteps(Recipe recipe) {
         this.recipe = recipe;
+        setupTimes();
+        setMiscs();
+
+
     }
+
+    private void setupTimes(){
+        this.boilTime = Integer.parseInt(recipe.getBOILTIME());
+        List<MASHSTEP> mashsteps = recipe.getMASH().getMASHSTEPS().getMASHSTEP();
+        int mashTime = 0;
+        if (mashsteps != null){
+            for (MASHSTEP mashstep : mashsteps){
+                if(mashstep.getSTEPTIME() != null){
+                    mashTime += Integer.parseInt(mashstep.getSTEPTIME());
+                }
+            }
+        }
+
+        this.mashTime = mashTime;
+    }
+
+    private void setMiscs(){
+        fermentMisc = new ArrayList<>();
+        mashMisc = new ArrayList<>();
+        boilMisc = new ArrayList<>();
+        for (MISC misc:recipe.getMISCS().getMISC()){
+            if(misc.getUSE().equals("Mash")){
+                mashMisc.add(misc);
+            }if(misc.getUSE().equals("Boil")){
+                boilMisc.add(misc);
+            }
+            if(misc.getUSE().equals("Secondary")){
+                fermentMisc.add(misc);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 
 
 }
